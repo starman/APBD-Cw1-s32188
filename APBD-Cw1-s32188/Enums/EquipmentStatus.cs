a4eee3054b9cd23a5b8a53e86e3eddc4543d8388
Jakub Ciemała <ciemalajakub@gmail.com>
2026-03-21 14:19:52 +0100

    Add base Equipment class

---
+++ APBD-Cw1-s32188/Enums/EquipmentStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace APBD_Cw1_s32188.Enums;
+
+public enum EquipmentStatus
+{
+    Available,
+    Unavailable
+}
---
+++ APBD-Cw1-s32188/Models/Equipment.cs
@@ -0,0 +1,14 @@
+﻿using APBD_Cw1_s32188.Enums;
+
+namespace APBD_Cw1_s32188.Models;
+
+public abstract class Equipment(string name, string manufacturer, int productionYear)
+{
+    private static int _nextId = 1;
+    
+    public int Id { get; } = _nextId++;
+    public string Name { get; set; } = name;
+    public string Manufacturer { get; set; } = manufacturer;
+    public int ProductionYear { get; set; } = productionYear;
+    public EquipmentStatus Status { get; set; } = EquipmentStatus.Available;
+}

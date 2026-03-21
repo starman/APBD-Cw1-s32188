dd3d92eb06237a92ec19bbe81580461601852e49
Jakub Ciemała <ciemalajakub@gmail.com>
2026-03-21 15:14:13 +0100

    Add Rental class

---
+++ APBD-Cw1-s32188/Models/Rental.cs
@@ -0,0 +1,13 @@
+﻿namespace APBD_Cw1_s32188.Models;
+
+public class Rental(User user, Equipment equipment, DateTime rentalDate, int days)
+{
+    private static int _nextId = 1;
+    
+    public int Id { get; set; } = _nextId++;
+    public User user { get; set; } = user;
+    public Equipment equipment { get; set; } = equipment;
+    public DateTime RentalDate { get; set; } = rentalDate;
+    public int Days { get; set; } = days;
+    public DateTime? ReturnDate { get; set; } = null;
+}

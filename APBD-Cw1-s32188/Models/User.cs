3fc03d10507574f699a4e1c9362fac190e2834dc
Jakub Ciemała <ciemalajakub@gmail.com>
2026-03-21 14:53:20 +0100

    Add base User class

---
+++ APBD-Cw1-s32188/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace APBD_Cw1_s32188.Models;
+
+public abstract class User(string firstName, string lastName)
+{
+    private static int _nextId = 1;
+    
+    public int Id { get; set; } = _nextId++;
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+}
